d081429e0616a2bd3ca35c9487894f8ca87d10e0
Luis Palomero <LPALOMERO@informatica.local>
2017-10-19 17:38:59 +0200

    Con todos los calculos terminados pero antes de ponerme a hacerlo bien

---
+++ Ejercicio1.R
@@ -0,0 +1,66 @@
+# Intentaré seguir los estándares de estilo de Google: https://google.github.io/styleguide/Rguide.xml
+#Antes de nada, vamos a organizar el entorno
+par(mfrow=c(2,2)) # vamos a presentar 4 gráficos
+
+# Cargamos el fichero antebrazos.txt. He tenido que retocarlo un poco porque incluía
+# saltos de línea y he puesto todos los datos en una única línea.
+
+longitudes<-scan("antebrazos.txt") # cargamos el ficher, tiene 140 datos
+
+# Con summary tenemos varios de los datos que nos piden. Tenemos que quitar cabeceras cada vez
+long.resumen<-summary(longitudes)
+long.media=long.resumen["Mean"]
+names(long.media)<-NULL
+long.mediana=long.resumen["Median"]
+names(long.mediana)<-NULL
+long.min=long.resumen["Min."]
+names(long.min)<-NULL
+long.max=long.resumen["Max."]
+names(long.max)<-NULL
+long.cuartil.1=long.resumen["1st Qu."]
+names(long.cuartil.1)<-NULL
+long.cuartil.3=long.resumen["3rd Qu."]
+names(long.cuartil.3)<-NULL
+
+#La desviación típica tiene su propia función
+long.desv.tipica=sd(longitudes)
+
+# Para la moda, E no tiene función específica (Olé tus naies para el mejor programa de estadística)
+# pero una úsqueda en google nos cuenta como construirnos la función apropiada
+
+ Moda<- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
+long.moda<-Moda(longitudes)
+
+#Coeficiente de asimetría de Pearson
+long.asimetria.Pearson<-100*long.desv.tipica/long.media
+
+long.recorrido<-long.max-long.min  # 5.3 de recorrido
+# Distribución de frecuencias absolutas. Partiremos los datos en intervalos
+# según el criterio de Sturges k = 1 + 3,322log10(n)
+intervalos<-round(1+3.322*log10(length(longitudes))) # 8 intervalos
+
+# Establecemos los puntos de rotura con el máximo y el mínimo redondeados y 
+# el número de intervalos calculados
+intervalo.min<- floor(long.min)
+intervalo.max<-ceiling(long.max)
+breaks<-seq(intervalo.min,intervalo.max,(intervalo.max-intervalo.min)/intervalos) # estos son los puntos de corte
+longitudes.cut<-cut(longitudes,breaks,right=FALSE)
+longitudes.freq.abs <- table(longitudes.cut) # y esta es la distribución de frecuencias absolutas
+longitudes.freq.relativa<-longitudes.freq.abs/length(longitudes)
+
+
+hist(longitudes,breaks,freq=FALSE, right=FALSE)
+
+#Frecuencias absolutas
+# Histograma de frecuencias RElatiacs X
+# Media X
+# mediana X
+# moda X
+# primer cuartil X
+# tercer cartil X
+# desviación típica X
+# recorrido X
+# coeficiente de asimetria de pearson X
